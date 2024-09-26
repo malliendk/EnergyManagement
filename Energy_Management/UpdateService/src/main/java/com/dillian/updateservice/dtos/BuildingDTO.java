3c99414fca0fcec730cfedb796906bc464ee0c86
DMALLIEN <malliendk@gmail.com>
2024-09-26 17:50:11 +0200

    Added repositories for subclasses, findAll methods, controllers. Building FindById for BFF

---
+++ Energy_Management/UpdateService/src/main/java/com/dillian/updateservice/dtos/BuildingDTO.java
@@ -0,0 +1,24 @@
+package com.dillian.updateservice.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BuildingDTO {
+
+    private Long id;
+    private String name;
+    private String description;
+    private int price;
+    private String imageUri;
+    private double gridCapacityIncrease;
+    private double gridLoad;
+    private double energyProduction;
+    private int houseHolds;
+    private double energyConsumption;
+    private int solarPanelCapacity;
+    private double popularityYield;
+    private int researchYield;
+}
+
